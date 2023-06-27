580d60ff13f14242b5d6f17f7ff460439a1e98d6
Angel Carrero <angel.carrero@correo.unimet.edu.ve>
2023-06-26 22:20:26 -0400

    insercion punto 3

--- Proyecto2_Carrero_Sisiruca_Machta/src/Ventanas/Imagen.java
+++ Proyecto2_Carrero_Sisiruca_Machta/src/Ventanas/Imagen.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Ventanas;
 import java.awt.Graphics;
@@ -30,4 +29,3 @@ public class Imagen extends javax.swing.JPanel {
     }    
 
 }
-
